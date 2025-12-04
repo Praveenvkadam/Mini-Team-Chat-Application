c790ce0f51327e4f52b072fc5b5d06ad834da2f0
Praveen Kadam <93198500+Praveenvkadam@users.noreply.github.com>
2025-12-04 21:42:38 +0530

    Remove unique indexes from User schema
    Removed unique indexes for username, email, and phone.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -55,8 +55,4 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-UserSchema.index({ username: 1 }, { unique: true });
-UserSchema.index({ email: 1 }, { unique: true });
-UserSchema.index({ phone: 1 }, { unique: true });
-
 module.exports = mongoose.model("User", UserSchema);
